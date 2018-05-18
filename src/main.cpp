5306c1a91cbc77396950591952e9a2f4d95a4a42
Chris0698 <chris.aston98@outlook.com>
2018-05-18 17:10:28 +0100

    Seminar Stuff

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,12 @@
 
 Serial pc(USBTX, USBRX);
 Thread worker;
+Thread anotherThread;
 EventQueue queue ;
 
 DigitalOut red(LED_RED,1); /* initial state 1 led is off */
+DigitalOut blue(D9, 0);
+
 void flash_red(void) {
     while(1){
         red.write(1);
@@ -15,11 +18,23 @@ void flash_red(void) {
         wait(0.5);
     }
 }
+
+void flash_top(void)
+{
+  while(true)
+  {
+    blue.write(0);
+    wait(0.5);
+    blue.write(1);
+    wait(0.5);
+  }
+}
+
 int main() {
 
     // put your setup code here, to run once:
     worker.start( flash_red );
-
+    anotherThread.start(flash_top);
     while(1) {
         // put your main code here, to run repeatedly:
     }
